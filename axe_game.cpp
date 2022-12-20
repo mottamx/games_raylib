c0bd88c2a3cdc21b3c777740991f32d6d31cdb9a
mottamx <cmottaa@gmail.com>
2022-12-20 10:50:41 -0600

    Simple game, raylibs basic functions
    Raylibs: Draw:Circle, rectangle, text. SetTargetFPS and very basics BeginDrawing, ClearBackground, EndDrawing.
    Collision detection with edges
    C++ without OOP

---
+++ axe_game.cpp
@@ -0,0 +1,79 @@
+//FROM COURSE GAMEDEV.TV
+//C++ Fundamentals: Game Programming For Beginners 
+#include "raylib.h"
+
+int main(){
+    //Window Dimensions
+    int width{800};
+    int height{450};
+    InitWindow(width,height,"Axe game");
+    //Circle coordinates
+    int circle_x{200};
+    int circle_y{200};
+    int circle_radius{25};
+    //Edges
+    int l_circle_x{circle_x-circle_radius};
+    int r_circle_x{circle_x+circle_radius};
+    int u_circle_y{circle_y-circle_radius};
+    int b_circle_y{circle_y+circle_radius};
+    //Axe coordinates
+    int axe_x{400};
+    int axe_y{0};
+    int axe_length{50};
+    int l_axe_x{axe_x};
+    int r_axe_x{axe_x+axe_length};
+    int u_axe_y{axe_y};
+    int b_axe_y{axe_y+axe_length};
+
+    int direction{10};
+    bool collision_with_axe=
+                    (b_axe_y>=u_circle_y)&&
+                    (u_axe_y<=b_circle_y)&&
+                    (r_axe_x>=l_circle_x)&&
+                    (l_axe_x<=r_circle_x);
+
+    SetTargetFPS(60);
+    while(!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground(WHITE);
+        //Game logic begins
+        //update edges
+        l_circle_x=circle_x-circle_radius;
+        r_circle_x=circle_x+circle_radius;
+        u_circle_y=circle_y-circle_radius;
+        b_circle_y=circle_y+circle_radius;
+        l_axe_x=axe_x;
+        r_axe_x=axe_x+axe_length;
+        u_axe_y=axe_y;
+        b_axe_y=axe_y+axe_length;
+        //Update collision
+        collision_with_axe=
+                    (b_axe_y>=u_circle_y)&&
+                    (u_axe_y<=b_circle_y)&&
+                    (r_axe_x>=l_circle_x)&&
+                    (l_axe_x<=r_circle_x);
+
+        if(collision_with_axe){
+            DrawText("Game Over", 400,200,20, RED);
+        }else{
+            DrawCircle(circle_x,circle_y,circle_radius, BLUE);
+            DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
+            
+            //Move axe
+            axe_y+=direction;
+            if(axe_y>height-axe_length || axe_y <0 ){
+                direction=-direction;
+            }
+
+            if(IsKeyDown(KEY_D) && circle_x<width ){
+                circle_x+=10;
+            }
+            if(IsKeyDown(KEY_A) && circle_x > 0){
+                circle_x-=10;
+            }
+        }
+        //Game logic ends
+        EndDrawing();
+    }
+
+}
